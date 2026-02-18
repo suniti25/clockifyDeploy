d903eab1ce4009181b275b12e4c371954f90b378
Twisu009 <59360203+Twisu009@users.noreply.github.com>
2026-02-18 13:06:32 +0545

    Twi/feat/deploy (#12)
    * fix : updated Dickerfile and docker-compose file
    
    * Merge main into twi/feat/deploy and apply staged changes

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-python manage.py migrate --noinput --settings=leave_management_system.settings
-python manage.py collectstatic --noinput --clear --settings=leave_management_system.settings
+python manage.py migrate --noinput --settings=leave_system.settings
+python manage.py collectstatic --noinput --clear --settings=leave_system.settings
 
 exec "$@"
